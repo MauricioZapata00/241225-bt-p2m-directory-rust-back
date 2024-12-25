a79cdd50a0e0c093a1b6349aac79eaeded553732
MauricioZapata00 <maurozac123@gmail.com>
2024-12-25 17:49:24 -0500

    Beginning of the project

---
+++ runner/src/main.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{State, http::Status, response::status, serde::json::Json};
+use domain::model::commerce::Commerce;
+use infrastructure::controller::commerce_controller::CommerceController;
+
+#[get("/commerces")]
+async fn list_commerces(controller: &State<CommerceController>) -> Result<Json<Vec<Commerce>>, Status> {
+    controller.get_commerce().await
+}
+
+#[launch]
+async fn rocket() -> rocket::Rocket<rocket::Build> {
+
+    let figment = rocket::Config::figment()
+        .merge(("port", 8008));
+    let commerce_controller = CommerceController::new();
+
+    rocket::custom(figment)
+        .manage(commerce_controller)
+        .mount("/", routes![list_commerces])
+}
