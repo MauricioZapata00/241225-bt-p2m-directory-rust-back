2e92f4fd5527726d66ede1fad3b847529a079213
MauricioZapata00 <maurozac123@gmail.com>
2025-01-28 09:55:53 -0500

    Added files to not be commited to .gitignore file

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -3,8 +3,24 @@ extern crate rocket;
 
 use rocket::{State, http::Status, response::status, serde::json::Json};
 use domain::model::commerce::Commerce;
+use infrastructure::db::mssql::banks::repository::bank_repository::SqlxBankRepository;
+use infrastructure::db::mssql::commerces::repository::account_repository::SqlxAccountRepository;
+use infrastructure::db::mssql::commerces::repository::commerce_repository::SqlxCommerceRepository;
+use infrastructure::db::mssql::commerces::repository::commerce_status_repository::SqlxCommerceStatusRepository;
 use infrastructure::entrypoint::commerces::commerce_controller::CommerceController;
 
+struct AppState {
+    sqlx_account_repository: SqlxAccountRepository,
+    sqlx_bank_repository: SqlxBankRepository,
+    sqlx_commerce_status_repository: SqlxCommerceStatusRepository,
+    sqlx_commerce_repository: SqlxCommerceRepository
+}
+
+impl AppState {
+    async fn new() -> Self {
+
+    }
+}
 #[get("/entrypoint")]
 async fn list_commerces(controller: &State<CommerceController>) -> Result<Json<Vec<Commerce>>, Status> {
     controller.get_commerce().await
