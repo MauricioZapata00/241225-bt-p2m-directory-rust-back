3a92851bbb353c45ce4cd8930004f1b85f75e074
MauricioZapata00 <maurozac123@gmail.com>
2025-02-14 23:39:47 -0500

    Fixed some issues left in the previous commit that were wrong and for that the application was not compiling

--- infrastructure/src/entrypoint/commerces/dto/account_dto.rs
+++ infrastructure/src/entrypoint/commerces/dto/account_dto.rs
@@ -1,5 +1,5 @@
+use std::fmt;
 use rocket::serde::{Deserialize as RocketDeserialize, Serialize as RocketSerialize};
-
 
 #[derive(Debug, Clone, RocketSerialize, RocketDeserialize)]
 pub struct AccountDto {
@@ -16,5 +16,16 @@ impl AccountDto {
             account_number,
             bank_code,
         }
+    }
+}
+
+impl fmt::Display for AccountDto {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Account {{ number: {}, bank_code: {} }}",
+            self.account_number,
+            self.bank_code
+        )
     }
 }
